e01229b910c3a8fcb43a63e7c3b073567b8426b2
nihar <zanwarnihar@gmail.com>
2020-04-05 17:01:47 +0530

    change install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ sleep 5
 git clone https://github.com/NiharZanwar/face-recog-server.git
 cd face-recog-server
 echo "clone complete"
-cd /data;
+cd /app/face-recog-server
 python app.py
+cd /data;
 python -m http.server 8008 &
 echo "http server runnong"
-cd /app/face-recog-server
 
